877e3bcb079d06c7d2f4feacf660bf99d4aa9cc2
Dimple-Kanwar <dimple.kanwar@wipro.com>
2016-12-08 12:23:06 +0000

    Update managePO.go

--- managePO/managePO.go
+++ managePO/managePO.go
@@ -282,20 +282,20 @@ func (t *ManagePO) get_AllPO(stub shim.ChaincodeStubInterface, args []string) ([
 	}
 	var jsonResp string
 	jsonResp = "{"
-	/*for i,val :=range poIndex2{
+	for i,val :=range poIndex2{
 		valAsbytes, err := stub.GetState(val)									//get the var from chaincode state
 		if err != nil {
-			jsonResp = "{\"Error\":\"Failed to get state for " + val + "\"}"
+			&jsonResp = "{\"Error\":\"Failed to get state for " + val + "\"}"
 			return nil, errors.New(jsonResp)
 		}
-		jsonResp = jsonResp + "\""+ val + "\":" + string(valAsbytes[:])
+		&jsonResp = jsonResp + "\""+ val + "\":" + string(valAsbytes[:])
 		
 		if i != 0 {
-			jsonResp = jsonResp + ","
+			&jsonResp = jsonResp + ","
 		}
-	}*/
-	valAsbytes, err := stub.GetState(poIndex[0])	
-	jsonResp = jsonResp + "\""+ poIndex[0] + "\":" + string(valAsbytes[:])
+	}
+	//valAsbytes, err := stub.GetState(poIndex[0])	
+	//jsonResp = jsonResp + "\""+ poIndex[0] + "\":" + string(valAsbytes[:])
 	jsonResp = jsonResp + "}"
 	
 	return []byte(jsonResp), nil													//send it onward
